051e7e2f55e1b8250cdde23943072185fabb31f9
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-05 08:53:32 -0300

    Atribuir a var ao texto interno do HTML.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function demoVisibility() {
     document.getElementById("mensagem").style.visibility = "hidden";
     var inputText = document.getElementById("mensagem").value;
-
+    document.getElementById("showText").innerText = inputText;
   }
