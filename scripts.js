a8de618e3960366ff1305d79ec3100e20a60234a
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-05 11:00:33 -0300

    Alterar valores de margens

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 function demoVisibility() {
     document.getElementById("mensagem").style.visibility = "hidden";
     var inputText = document.getElementById("mensagem").value;
-
-    if(inputText.style.dispaly === "none"){
-       document.getElementById("showText").innerText = inputText;
-    }
-
+    document.getElementById("showText").innerText = inputText;
   }
 
 
