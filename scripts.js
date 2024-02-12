31a100466afd23de4888a820dcac83a7d686f327
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-12 08:34:43 -0300

    Atualizar projeto e esconder botao

--- scripts.js
+++ scripts.js
@@ -3,16 +3,20 @@ function demoVisibility() {
     document.getElementById("mensagem").style.visibility = "hidden";
     var inputText = document.getElementById("mensagem").value;
     document.getElementById("showText").innerText = inputText;
+    var buttonElement = document.getElementById("myButton");
+    buttonElement.style.visibility = "visible";
+
   }
-
-
+ 
   function myFunction() {
     var x = document.getElementById('myDIV');
     if (x.style.display === 'none') {
       x.style.display = 'none';
+
     } else {
       x.style.display = "none";
       demoVisibility() 
+     
     }
   }
   
