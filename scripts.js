9f680a31393fc3428d4e238a11e7cf5ae2df6d78
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-13 07:25:44 -0300

    Criar um condicao quando o botao for visivel.

--- scripts.js
+++ scripts.js
@@ -3,9 +3,12 @@ function demoVisibility() {
     document.getElementById("mensagem").style.visibility = "hidden";
     var inputText = document.getElementById("mensagem").value;
     document.getElementById("showText").innerText = inputText;
+
     var buttonElement = document.getElementById("myButton");
     buttonElement.style.visibility = "visible";
-
+    if (buttonElement.style.visibility === `visible`){
+     buttonElement.style.marginTop = `781px`
+    }
   }
  
   function myFunction() {
