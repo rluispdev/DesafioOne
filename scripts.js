69689ef43f89b09dd21514dae02697bc67fb6100
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-17 13:46:37 -0300

    Criar funcoes para converter string em bin e vice versa.

--- scripts.js
+++ scripts.js
@@ -19,11 +19,39 @@ function showCryptoText(textCrypt) {
   showTextElement.innerText = textCrypt;
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
   encodedText();  
 });
 
+function processText(cryptoFunction) {
+  var inputText = document.getElementById("message").value;
+  document.getElementById("showText").innerText = inputText;
+  let cryptoResult = cryptoFunction(inputText);
+  showCryptoText(cryptoResult);
+}
+
+//Srting -> Bin
+const stringToBinary = (str) => {
+  let binary = "";
+  for (let i = 0; i < str.length; i++) {
+      const charBinary = str[i].charCodeAt(0).toString(2); // Adicione 0 como parâmetro
+      binary += charBinary.padStart(8, '0');
+  }
+  return binary;
+};
+
+//Bin -> String
+function bin2text(bin) {
+  bin = bin.split(""); // Remova o espaço aqui
+  let result = "";
+  for (let i = 0; i < bin.length; i += 8) {
+    let chunk = bin.slice(i, i + 8).join(""); // Pegue cada pedaço de 8 dígitos binários
+    result += String.fromCharCode(parseInt(chunk, 2)); // Converta para caractere
+  }
+  return result;
+};
+
+//Func MasterControl
 function toggleVisibility(action) {
   var displayVisibility = document.getElementById('myDIV');
   if (displayVisibility.style.display === 'none' || action === 'hide') {
@@ -32,23 +60,11 @@ function toggleVisibility(action) {
     displayVisibility.style.display = 'none';
     elementVisibility();
     if (action === 'encode') {
-      processText(window.btoa);
-   
+      processText(stringToBinary)
     } else if (action === 'decode') {
-      processText(window.atob);
+      processText(bin2text)
     }
   }
 }
 
-function processText(cryptoFunction) {
-  var inputText = document.getElementById("message").value;
-  document.getElementById("showText").innerText = inputText;
-  let cryptoResult = cryptoFunction(inputText);
-  showCryptoText(cryptoResult);
-  
-}
 
- 
-
-
- 
